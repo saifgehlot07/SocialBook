bb690b3a895378105d65d7c97f3e52abbee5dda7
saif gehlot <saifgehlot07@gmail.com>
2022-05-29 20:18:07 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// SideBar
+
+const menuItems = document.querySelectorAll(".menu-item");
+
+// remove active class from all menu items
+const changeActiveItem = () => {
+  menuItems.forEach((item) => {
+    item.classList.remove("active");
+  });
+};
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    changeActiveItem();
+    item.classList.add("active");
+    if (item.id != "notifications") {
+      document.querySelector(".notification-popup").style.display = "none";
+    } else {
+      document.querySelector(".notification-popup").style.display = "block";
+    }
+  });
+});
